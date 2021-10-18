feeab58c4bee922a769ae021ae2e69176b7e4c5a
Grégoire <gbadiche@gmail.com>
2021-10-18 22:21:12 +0200

    changed code ;)

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ The main code file.
 On va essayer de faire de la programmation modulaire (avec des modules dcp) ca va être plus pratique.
 """
 
+import os
+
 def main():
     print("oue bg tkt")
+    os.system("mode 50, 20") # Set up terminal size to 50x20
+    input("Entrez n'importe quoi pour quitter")
 
 if __name__ == "__main__":
     main()
