87548d9c543414da0b695e94ed7c762f731eecbe
Grégoire <gbadiche@gmail.com>
2021-10-18 22:09:13 +0200

    created first script

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ The main code file.
 
 On va essayer de faire de la programmation modulaire (avec des modules dcp) ca va être plus pratique.
 """
+
+def main():
+    print("oue bg tkt")
+
+if __name__ == "__main__":
+    main()
